893594971b0d9d090b38d80a8f26599bcd8b239c
Frank Massi <frankmassi@gmail.com>
2019-07-29 11:01:21 -0500

    add in stats stuff, refactor BudgetEvent modeling a bit

--- budgie/budgie/budget_event.py
+++ budgie/budgie/budget_event.py
@@ -1,13 +1,21 @@
 from budgie.frequency import FREQUENCY_TYPES
 
+from collections import namedtuple
 from datetime import datetime, timedelta
 from scipy.stats import skewnorm
+from statistics import mean
 
+
+Event = namedtuple('Event', 'event_date event_amounts')
+EventSummary = namedtuple('EventSummary', 'event_date min mean max')
+
+DAYS_PER_YEAR=365
+DEFAULT_SAMPLES=100
 
 class BudgetEvent:
 
     def __init__(self, amount, frequency_type, start_date=None,
-                 end_date=None, credit=True, stdev=0, skew=0):
+                 end_date=None, credit=True, stdev=0, skew=0, run_days=DAYS_PER_YEAR, samples=DEFAULT_SAMPLES):
         if credit:
             self.amount = amount * -1
         else:
@@ -17,38 +25,39 @@ class BudgetEvent:
         self.event_dates = freq.generate_event_dates()
         self.stdev = stdev
         self.skew = skew
+        self.run_days = run_days
+        if self.stdev:
+            self.samples = samples
+        else:
+            self.samples = 1
 
         self.data = None
+        self.summary = None
         self.generate_data()
+        self.generate_summary_data()
 
     def generate_event_amount(self):
         return skewnorm.rvs(self.skew, loc=self.amount, scale=self.stdev)
 
-    def generate_run(self):
-        run = []
-        running_amount = 0
-        for i in range(365):
-            the_date = (datetime.today() + timedelta(days=i)).date()
-            if the_date in self.event_dates:
-                new_amount = self.generate_event_amount()
-                running_amount = running_amount + new_amount
-
-            run.append(running_amount)
-
-        return run
-
     def generate_data(self):
         if not self.data:
             data = []
-            # TODO: clean this up
-            if self.stdev:
-                for i in range(100):
-                    data.append(self.generate_run())
+            for i in range(self.run_days):
+                event_date = (datetime.today() + timedelta(days=i)).date()
+                if event_date in self.event_dates:
+                    data.append(Event(event_date, self.generate_event_data()))
 
-            else:
-                run = self.generate_run()
-                for i in range(100):
-                    data.append(run)
             self.data = data
 
-        return self.data
+    def generate_event_data(self):
+        return [self.generate_event_amount() for i in range(self.samples)]
+
+
+    def generate_summary_data(self):
+        if not self.summary:
+            summary = []
+            for event in self.data:
+                # TODO: maybe flip min/max if credit?
+                summary.append(EventSummary(event.event_date, min(event.event_amounts), mean(event.event_amounts), max(event.event_amounts)))
+
+        self.summary = summary
